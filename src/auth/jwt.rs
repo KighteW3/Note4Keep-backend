b89e23e1a0c2201ff916aa8c5ce0b528dbffcd3a
kighteW3 <kighte04@gmail.com>
2023-12-09 01:26:48 +0100

    added jwt token creator function

--- src/auth/jwt.rs
+++ src/auth/jwt.rs
@@ -1,20 +1,46 @@
-use chrono::prelude::*;
-use jsonwebtoken::{
-    decode, encode, get_current_timestamp, Algorithm, DecodingKey, EncodingKey, Header, Validation,
-};
+use std::env;
 
+use chrono::{prelude::*, Duration};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 struct Claims {
     username: String,
-    user_id: String,
+    userid: String,
     email: String,
-    iat: DateTime<Utc>,
+    iat: i64,
+    exp: i64,
 }
 
-pub async fn create_jwt(user: String, id: String, email: String) {
+pub async fn create_jwt(
+    user: String,
+    id: String,
+    email: String,
+) -> Result<String, jsonwebtoken::errors::Error> {
+    let secret = env::var("SECRET").unwrap();
+
     let token_structure = Claims {
         username: user,
-        user_id: id,
-        email: email,
-        iat: Utc::now(),
+        userid: id,
+        email,
+        iat: Utc::now()
+            .checked_add_signed(Duration::seconds(60))
+            .expect("valid timestamp")
+            .timestamp(),
+        exp: Utc
+            .with_ymd_and_hms(Utc::now().year() + 1, 10, 10, 10, 10, 10)
+            .unwrap()
+            .timestamp(),
     };
+
+    let token = encode(
+        &Header::default(),
+        &token_structure,
+        &EncodingKey::from_secret(&secret.as_ref()),
+    );
+
+    token
 }
+
+pub async fn compare_jwt() {}
