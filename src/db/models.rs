edd53f72f5119c4ffae9a2f4c6828bcc1bd62295
kighte143 <kighte04@gmail.com>
2024-01-30 12:35:34 +0100

    Initializating userOptions functions

--- src/db/models.rs
+++ src/db/models.rs
@@ -1,6 +1,13 @@
-use bson::serde_helpers::chrono_datetime_as_bson_datetime;
+use alloc::task;
+use bson::{doc, serde_helpers::chrono_datetime_as_bson_datetime};
 use chrono::prelude::*;
+use hyper::StatusCode;
+use jsonwebtoken::TokenData;
+use mongodb::Collection;
 use serde::{Deserialize, Serialize};
+use tokio::runtime::Handle;
+
+use crate::auth::jwt::Claims;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
@@ -23,7 +30,55 @@ pub struct Notes {
 }
 
 // Pending to decide:
-/* #[derive(Debug, Serialize, Deserialize)]
-pub struct userOptions {
-    pub
-} */
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserOptions {
+    picture: String,
+    theme: String,
+    filter_by_name: String,
+    filter_by_prio: String,
+}
+
+pub enum Errors {
+    Mongo(mongodb::error::Error),
+    NotFound(StatusCode),
+}
+
+impl UserOptions {
+    pub fn create(
+        &self,
+        user: &String,
+        coll: Collection<UserOptions>,
+        claims: TokenData<Claims>,
+    ) -> Result<String, Errors> {
+        task::block_in_place(move || {
+            Handle::current().block_on(async move {
+                let coll = &coll;
+
+                let filters = doc! {"user": &user};
+
+                let exists = match coll.find_one(filters, None).await {
+                    Ok(res) => match res {
+                        Some(res2) => res2,
+                        None => return Err(Errors::NotFound(StatusCode::NOT_FOUND)),
+                    },
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                        return Err(Errors::Mongo(e));
+                    }
+                };
+
+                // const data = UserOptions {}
+
+                let created = match coll.insert_one(data, None).await {
+                    Ok(res) => res,
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                        return Err(Errors::Mongo(e));
+                    }
+                };
+
+                Ok(String::from("adawd"))
+            })
+        })
+    }
+}
